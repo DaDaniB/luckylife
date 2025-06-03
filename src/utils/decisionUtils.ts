5e62eb4cd846321e5ef76d2fde2d8867a552b03d
baumgartinger <daniel.baumgartinger@teamviewer.com>
2025-06-04 00:57:05 +0200

    bilder bugfix

--- src/utils/decisionUtils.ts
+++ src/utils/decisionUtils.ts
@@ -1,3 +1,4 @@
+import type { DisplayedText } from '../components/ResultPage';
 import rawDecisionData from '../data/decision.json';
 import type { DecisionTree } from '../types';
 
@@ -8,12 +9,31 @@ export const getFilteredAnswers = (
   selectedAnswers: Record<string, string>
 ) => {
   const section = decisionData.sections[sectionIndex];
-  return section.questions.map((question) => {
-    const filteredAnswers = question.answers.filter((answer) => {
-      const isExcluded = answer.excludes?.some((ex: string) => Object.values(selectedAnswers).includes(ex));
-      const isIncluded = answer.includes?.every((inc: string) => Object.values(selectedAnswers).includes(inc));
-      return !isExcluded && (!answer.includes || isIncluded);
-    });
-    return { question: question.question, answers: filteredAnswers };
-  });
+
+  const returnedValues: DisplayedText[] = []
+  for (let question of section.questions) {
+    let possibleAnswers = []
+    for (let answer of question.answers) {
+      const isExcluded = answer.excludes?.some((ex: string) => {
+        return Object.values(selectedAnswers).includes(ex) || returnedValues.map(val => val.answer).includes(ex)
+      })
+      const isIncluded = answer.includes?.every((inc: string) => {
+        return Object.values(selectedAnswers).includes(inc) || returnedValues.map(val => val.answer).includes(inc);
+      })
+
+      if (!isExcluded && (!answer.includes || isIncluded)) {
+        possibleAnswers.push(answer)
+      }
+    }
+    const i = Math.floor(Math.random() * possibleAnswers.length);
+    const a = possibleAnswers[i];
+    let selectedAnswer: DisplayedText;
+    if (a) {
+      selectedAnswer = { question: question.question, answer: a.answer, isEnd: a.endflow ?? false };
+    } else {
+      selectedAnswer = { question: "", answer: "", isEnd: false };
+    }
+    returnedValues.push(selectedAnswer)
+  }
+  return returnedValues
 };
